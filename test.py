ff1ee835268ec9236bbbb2fda45a13354be20afa
Artur Kokoszka <akokoszka@student.agh.edu.pl>
2021-01-06 17:53:52 +0100

    dsfdsf

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, jsonify
-import project.__init__
+import project.__init__.mysql
 
 testblueprint = Blueprint('testblueprint', __name__)
 
 @testblueprint.route('/test')
 def test():
+    conn = mysql.connect()
+    cursor = conn.cursor()
     SQL="select * from accounts"
     cursor.execute(SQL)
     data=cursor.fetchone()
