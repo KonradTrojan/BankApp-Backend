ecc75b7580c26c2b568c16302c502d2465281b04
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-09 12:26:15 +0100

    testowanie bindowania 2

--- accounts.py
+++ accounts.py
@@ -18,11 +18,14 @@ def accounts():
 def accountsForId(id):
     if request.method == 'POST' and 'name' in request.form:
         cursor = mysql.get_db().cursor()
+        name = request.form['name']
         now = datetime.today()
         balance = 0
+        SQL = """INSERT INTO accounts (balance, dataOpened, name) VALUES (:balance, :dataOPened, :name)"""
+        cursor.execute(SQL, [balance, now, name])
     else:
         cursor = mysql.get_db().cursor()
-        SQL="select * from accounts where idAccounts="+str(id)
+        SQL="select * from accounts where idAccounts=+str(id)"
         cursor.execute(SQL)
         data=cursor.fetchall()
         resp=jsonify(data)
