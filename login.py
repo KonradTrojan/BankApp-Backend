a6900e96747cbbe26990e93a2445ce080068fbb9
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-09 15:36:54 +0100

    testowanie logowania

--- login.py
+++ login.py
@@ -5,19 +5,47 @@ from flask import (
     request,
     session,
     url_for,
-    Blueprint
+    Blueprint,
+    jsonify
 )
+from . import mysql
 
 loginblueprint = Blueprint('loginblueprint',__name__)
 
 @loginblueprint.route("/login",method = 'POST')
 def login():
+    cursor = mysql.get_db().cursor()
     if request.method == 'POST':
         session.pop('user_id', None)
         username = request.form['username']
         password = request.form['password']
 
-        sql = """select login from customers where login = :username """
+        #TODO  dać jakieś porządne bindowanie do sql poniżej
+        sql = "select login from customers where login = " + str(username)
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        passwordFromDB = data[7]
+        userID = data[1]
+
+    else:
+        sql = "select login from customers where login = " + 1
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        passwordFromDB = data[7]
+        userID = data[1]
+
+        return passwordFromDB
+
+        '''
+       
+        if passwordFromDB == password:
+            session['userID'] = userID
+            resp = jsonify(success=True)
+            return resp
+        else:
+            resp = jsonify(success=False)
+            return resp
+         '''
 
     return True
 
