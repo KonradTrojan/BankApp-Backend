d2b3ee4e8caa7cd13f25745270b4c27d6a82cc83
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-09 16:08:38 +0100

    testowanie logowania

--- login.py
+++ login.py
@@ -24,7 +24,7 @@ def login():
         sql = "select login from customers where login = " + str(username)
         cursor.execute(sql)
         data = cursor.fetchall()
-        passwordFromDB = data[7]
+        passwordFromDB = data['password']
         userID = data[1]
 
     else:
@@ -33,8 +33,8 @@ def login():
         cursor.execute(sql, [trojan])
         data = cursor.fetchall()
         resp = jsonify(data)
-        print(data)
-        return resp
+
+        return data['password']
 
         '''
        
