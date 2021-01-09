0e6d9a91121c76b55d72f23d80b9027cd81d2654
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-09 12:10:24 +0100

    testowanie bindowania

--- accounts.py
+++ accounts.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from . import mysql
+from datetime import datetime
 
 accountsblueprint = Blueprint('accountsblueprint', __name__)
 
@@ -12,22 +13,17 @@ def accounts():
     resp=jsonify(data)
     return resp
 
-@accountsblueprint.route('/accounts/<int:id>')
+
+@accountsblueprint.route('/accounts/<int:id>', methods=['GET', 'POST'])
 def accountsForId(id):
-    cursor = mysql.get_db().cursor()
-
-    SQL="select * from accounts where idAccounts="+str(id) 
-    cursor.execute(SQL)
-    data=cursor.fetchall()
-    resp=jsonify(data)
-    return resp
-
-@accountsblueprint.route('/accounts1/<int:id>', methods=['GET'])
-def test1(id):
-    cursor = mysql.get_db().cursor()
-
-    SQL="select * from accounts where idAccounts="+str(id) 
-    cursor.execute(SQL)
-    data=cursor.fetchall()
-    resp=jsonify(data)
-    return resp
+    if request.method == 'POST' and 'name' in request.form:
+        cursor = mysql.get_db().cursor()
+        now = datetime.today()
+        balance = 0
+    else:
+        cursor = mysql.get_db().cursor()
+        SQL="select * from accounts where idAccounts= :id"
+        cursor.execute(SQL, [id])
+        data=cursor.fetchall()
+        resp=jsonify(data)
+        return resp
