2205c6d1db9eb7194b735de72e428c8e24832eeb
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-10 12:25:35 +0100

    testowanie logowania

--- customers.py
+++ customers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, session
 from . import mysql
 from datetime import datetime
 
@@ -8,17 +8,24 @@ customersblueprint = Blueprint('customersblueprint', __name__)
 @customersblueprint.route('/customers')
 def customers():
     cursor = mysql.get_db().cursor()
-    sql = """select * from customers"""
+    sql = "select * from customers"
     cursor.execute(sql)
     data = cursor.fetchall()
     resp = jsonify(data)
     return resp
 
 
-@customersblueprint.route('/customers/<int:id>', methods=['GET', 'POST', 'PUT'])
+@customersblueprint.route('/customers/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def customersForId(id):
     if request.method == 'PUT':
         cursor = mysql.get_db().cursor()
+
+    elif request.method == 'DELETE':
+        cursor = mysql.get_db().cursor()
+        sql = """delete from customers where id = %d """
+        if cursor.execute(sql, [id]):
+            return 200
+
 
 
     else:
@@ -28,7 +35,3 @@ def customersForId(id):
         data = cursor.fetchall()
         resp = jsonify(data)
         return resp
-
-
-
-
--- login.py
+++ login.py
@@ -15,24 +15,31 @@ loginblueprint = Blueprint('loginblueprint',__name__)
 @loginblueprint.route("/login",methods = ['POST','GET'])
 def login():
     cursor = mysql.get_db().cursor()
-    if request.method == 'POST':
-        session.pop('user_id', None)
+    if request.method == 'GET':
+        session.pop('userId', None)
         username = request.form['username']
         password = request.form['password']
-
-        sql = """select login, password from customers where login like %s"""
+        username = "trojan"
+        sql = """select idCustomers, password from customers where login like %s"""
         cursor.execute(sql, [username])
-        rows = cursor.fetchall()
-        userID = rows[0][0]
-        password_ = rows[0][1]
+        rows = cursor.fetchone()
+        userID = rows[0]
+        password_ = rows[1]
 
+        session['userId'] = userID
+        resp = jsonify(success=True)
+        return userID
+
+
+        '''
         if password_ == password:
-            session['userID'] = userID
+            session['userId'] = userID
             resp = jsonify(success=True)
             return resp
         else:
             resp = jsonify(success=False)
             return resp
+        '''
 
 
 
