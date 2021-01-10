c086270674eb00bbcf8d779a665c9f78ba53b0e9
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-10 12:29:54 +0100

    testowanie logowania

--- login.py
+++ login.py
@@ -15,20 +15,20 @@ loginblueprint = Blueprint('loginblueprint',__name__)
 @loginblueprint.route("/login",methods = ['POST','GET'])
 def login():
     cursor = mysql.get_db().cursor()
-    if request.method == 'GET':
-        session.pop('userId', None)
-        username = request.form['username']
-        password = request.form['password']
-        username = "trojan"
-        sql = """select idCustomers, password from customers where login like %s"""
-        cursor.execute(sql, [username])
-        rows = cursor.fetchone()
-        userID = rows[0]
-        password_ = rows[1]
+    #if request.method == 'GET':
+    #session.pop('userId', None)
+    #username = request.form['username']
+    #password = request.form['password']
+    username = "trojan"
+    sql = """select idCustomers, password from customers where login like %s"""
+    cursor.execute(sql, [username])
+    rows = cursor.fetchone()
+    userID = rows[0]
+    password_ = rows[1]
 
-        session['userId'] = userID
-        resp = jsonify(success=True)
-        return userID
+    session['userId'] = userID
+    resp = jsonify(success=True)
+    return userID
 
 
         '''
