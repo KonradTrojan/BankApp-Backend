451598cdfd32f7689fec3902009fd9ad380f99e8
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-11 22:45:06 +0100

    dodanie usuwanie do accounts

--- accounts.py
+++ accounts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, session
 from . import mysql
 from datetime import datetime
 
@@ -14,15 +14,24 @@ def accounts():
     return resp
 
 
-@accountsblueprint.route('/accounts/<int:id>', methods=['GET', 'POST'])
+@accountsblueprint.route('/accounts/<int:id>', methods=['GET', 'POST', 'DELETE'])
 def accountsForId(id):
     if request.method == 'POST' and 'name' in request.form:
         cursor = mysql.get_db().cursor()
         name = request.form['name']
         now = datetime.today()
         balance = 0
-        SQL = """INSERT INTO accounts (balance, dataOpened, name) VALUES (:balance, :dataOPened, :name)"""
-        cursor.execute(SQL, [balance, now, name])
+        sql = """INSERT INTO accounts (balance, dataOpened, name) VALUES (:balance, :dataOPened, :name)"""
+        cursor.execute(sql, [balance, now, name])
+    if request.method == 'DELETE':
+        cursor = mysql.get_db().cursor()
+        sql = """SELECT idAccounts FROM owners WHERE idCustomers = %d"""
+        cursor.execute(sql, [session['userId']])
+        data = cursor.fetchone()
+        idAccounts = data[0]
+        sql = """DELETE FROM accounts WHERE idAccounts = %d"""
+        cursor.execute(sql, [idAccounts])
+        cursor.close()
     else:
         cursor = mysql.get_db().cursor()
         SQL="select * from accounts where idAccounts="+str(id)
