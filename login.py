d105ee9bf506fc2664cbddb61701ee88da4ff08a
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-12 17:57:35 +0100

    funkcjonalności login działają - dodanie obsługiwania http response

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request,session,url_for,Blueprint,jsonify,Response
+from flask import Flask, redirect, render_template, request,session,url_for,Blueprint,jsonify,Response,json
 
 
 from . import mysql
@@ -14,65 +14,18 @@ def login():
         sql = """select idCustomers, password from customers where login like %s"""
         cursor.execute(sql, [username])
 
-        # TODO dodać szyfrowanie haseł WSZĘDZIE
+        # TODO dodać szyfrowanie hashowanie WSZĘDZIE
         data = cursor.fetchone()
-        userID = data[0]
+        userId = data[0]
         password_ = data[1]
 
         password = request.form['password']
         if password_ == password:
-            session['userId'] = userID
-            # TODO zdecydować się na jeden sposób przesyłania statusów
+            session['userId'] = userId
 
-            return "udane logowanko"
+            return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
         else:
-            # TODO zdecydować się na jeden sposób przesyłania statusów
-
-            return "nieudane logowanko"
-'''
-@loginblueprint.route("/login1",methods = ['POST','GET'])
-def loginTest():
-
-
-
-    session.pop('userId', None)
-
-    username = "trojan"
-    cursor = mysql.get_db().cursor()
-    sql = """select idCustomers, password from customers where login like %s"""
-    cursor.execute(sql, [username])
-
-
-    # TODO dodać szyfrowanie haseł WSZĘDZIE
-    data = cursor.fetchone()
-    if data[0]  and data[1] is not None:
-        userID = data[0]
-        password_ = data[1]
-        password =password_
-        if password_ == password:
-            session['userId'] = userID
-            # TODO zdecydować się na jeden sposób przesyłania statusów
-            resp = jsonify(success=True)
-            return "udane logowanko"
-        else:
-            # TODO zdecydować się na jeden sposób przesyłania statusów
-            resp = jsonify(success=False)
-            return "nieudane logowanko"
-    else:
-        return "blad"
-@loginblueprint.route("/logout", methods = ['POST'])
-def logout():
-    if request.method == 'POST':
-        if 'userID' in session:
-            session.pop("userID")
-            content = {'please move along': 'nothing to see here'}
-            # TODO zdecydować się na jeden sposób przesyłania statusów
-            statusCode = Response(status=200)
-            return statusCode
-'''
-
-
-
+            return json.dumps({'success': False}), 200, {'ContentType': 'application/json'}
 
 
 
