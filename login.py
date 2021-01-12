eef86ba8f6ad6233e307505c45aa3adae70735bc
TrololoManPL <trololomanpl@mailplus.pl>
2021-01-12 21:08:58 +0100

    jwt

--- login.py
+++ login.py
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask, redirect, render_template, request,session,url_for,Blueprint,jsonify,Response,json
-
-
-from . import mysql
-
-loginblueprint = Blueprint('loginblueprint', __name__)
-@loginblueprint.route("/login",methods = ['POST'])
-def login():
-    if request.method == 'POST':
-        session.pop('userId', None)
-        username = request.json['username']
-
-        cursor = mysql.get_db().cursor()
-        sql = """select idCustomers, password from customers where login like %s"""
-        cursor.execute(sql, [username])
-
-        # TODO dodać szyfrowanie hashowanie WSZĘDZIE
-        data = cursor.fetchone()
-        userId = data[0]
-        password_ = data[1]
-
-        password = request.json['password']
-        if password_ == password:
-            session['userId'] = userId
-            return jsonify(token='test')
-        else:
-            return json.dumps({'success': False}), 200, {'ContentType': 'application/json'}
-
-
-
-
-
-
-
-
-=======
 from flask import Flask, redirect, render_template, request,session,url_for,Blueprint,jsonify,Response,json
 from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
@@ -97,5 +60,3 @@ def loginJWT():
 
 
 
-
->>>>>>> 393d128ca3073c29a190a647f513dd7473b685dd
