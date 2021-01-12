8c845ceb54c515d1cd91014e1091e46614120e87
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-12 12:10:37 +0100

    testowanie usuwania z tabeli accounts

--- accounts.py
+++ accounts.py
@@ -35,7 +35,7 @@ def accountsForId(id):
     else:
         cursor = mysql.get_db().cursor()
         sql = """select * from accounts where idAccounts= '%d'"""
-        cursor.execute("select * from accounts where idAccounts= '%d'", id)
+        cursor.execute("select * from accounts where idAccounts= '%d'", int(id))
         data = cursor.fetchall()
         resp = jsonify(data)
         return resp
