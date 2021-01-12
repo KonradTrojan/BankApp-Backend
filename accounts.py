89bedf0166a9364cd2c03e0b2f3ac0d2786ef685
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-12 11:54:01 +0100

    testowanie usuwania z tabeli accounts

--- accounts.py
+++ accounts.py
@@ -7,10 +7,10 @@ accountsblueprint = Blueprint('accountsblueprint', __name__)
 @accountsblueprint.route('/accounts')
 def accounts():
     cursor = mysql.get_db().cursor()
-    SQL="select * from accounts"
-    cursor.execute(SQL)
-    data=cursor.fetchall()
-    resp=jsonify(data)
+    sql = """select * from accounts"""
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    resp = jsonify(data)
     return resp
 
 
@@ -34,8 +34,41 @@ def accountsForId(id):
         cursor.close()
     else:
         cursor = mysql.get_db().cursor()
-        SQL="select * from accounts where idAccounts="+str(id)
-        cursor.execute(SQL)
-        data=cursor.fetchall()
-        resp=jsonify(data)
+        sql = """select * from accounts where idAccounts= :idAcc"""
+        cursor.execute(sql, [id])
+        data = cursor.fetchall()
+        resp = jsonify(data)
         return resp
+
+@accountsblueprint.route('/accountsTest/<int:id>', methods=['GET', 'POST', 'DELETE'])
+def accountsForIdTest(id):
+    '''
+    if request.method == 'POST':
+        cursor = mysql.get_db().cursor()
+        name = request.form['name']
+        now = datetime.today()
+        balance = 0
+        sql = """INSERT INTO accounts (balance, dataOpened, name) VALUES (:balance, :dataOPened, :name)"""
+        cursor.execute(sql, [balance, now, name])
+    '''
+
+    #if request.method == 'DELETE':
+    cursor = mysql.get_db().cursor()
+    session['userId'] = 3
+    sql = """SELECT idAccounts FROM owners WHERE idCustomers = %d"""
+    cursor.execute(sql, [session['userId']])
+    data = cursor.fetchone()
+    idAccounts = data[0]
+    sql = """DELETE FROM accounts WHERE idAccounts = %d"""
+    cursor.execute(sql, [idAccounts])
+    cursor.close()
+    return "usunieto"
+    '''
+    else:
+        cursor = mysql.get_db().cursor()
+        sql = """select * from accounts where idAccounts= :idAcc"""
+        cursor.execute(sql, [id])
+        data = cursor.fetchall()
+        resp = jsonify(data)
+        return resp
+    '''
