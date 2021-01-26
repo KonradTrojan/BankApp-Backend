4601bdb24e243bda1d26e3d46407bdcdf0f66184
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-27 00:15:51 +0100

    test

--- credit_cards.py
+++ credit_cards.py
@@ -13,11 +13,4 @@ def credit_cards():
     resp = jsonify(data)
     return resp
 
-@credit_cardsblueprint.route('/credit_cards')
-def credit_cards():
-    cursor = mysql.get_db().cursor()
-    SQL = "select * from credit_cards"
-    cursor.execute(SQL)
-    data = cursor.fetchall()
-    resp = jsonify(data)
-    return resp
+
