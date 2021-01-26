a6c0bc31ad0c0d33a19e2d30c7efae781f7f54a0
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-27 00:06:42 +0100

    test

--- credit_cards.py
+++ credit_cards.py
@@ -3,12 +3,21 @@ from . import mysql
 
 credit_cardsblueprint = Blueprint('credit_cardsblueprint', __name__)
 
+
 @credit_cardsblueprint.route('/credit_cards')
 def credit_cards():
     cursor = mysql.get_db().cursor()
-    SQL="select * from credit_cards"
+    SQL = "select * from credit_cards"
     cursor.execute(SQL)
     data = cursor.fetchall()
     resp = jsonify(data)
     return resp
-    
+
+@credit_cardsblueprint.route('/credit_cards')
+def credit_cards():
+    cursor = mysql.get_db().cursor()
+    SQL = "select * from credit_cards"
+    cursor.execute(SQL)
+    data = cursor.fetchall()
+    resp = jsonify(data)
+    return resp
