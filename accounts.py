415702d59071c005eed97ab73e4acc97a7d5501b
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-27 15:58:02 +0100

    testowanie połączenia z bd

--- accounts.py
+++ accounts.py
@@ -2,40 +2,14 @@ from flask import Blueprint, jsonify, request, session, json
 from . import mysql
 from datetime import datetime
 from project.jwtHandler import jwt, blacklist
-
+from flask_jwt_extended import jwt_required, get_jwt_claims
 accountsblueprint = Blueprint('accountsblueprint', __name__)
 
 @accountsblueprint.route('/accounts1')
 def accounts1():
-    identity = 1
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    sql = """select idAccounts from owners where idCustomers= %s """
-    cursor.execute(sql, [identity])
-    data = cursor.fetchall()
-    accountsTable = []
+    claims = get_jwt_claims()
+    return claims, 200
 
-    for row in data:
-        accountsTable.append(row[0])
-
-    myJson = []
-    for id in accountsTable:
-        try:
-            sql = """select number, dataOpened, balance from accounts where idAccounts= %s """
-            cursor.execute(sql, [id])
-            data = cursor.fetchone()
-            userData = []
-            for row in data:
-                userData.append(row)
-
-            myJson.append({
-                'idAccounts': id,
-                'number': userData[0],
-                'dataOpened': userData[1],
-                'balance': userData[2]
-            })
-        except TypeError:
-            print("testerrror")
 
     return jsonify(myJson)
 
@@ -92,19 +66,28 @@ def add_claims_to_access_token(identity):
     cursor = conn.cursor()
     sql = """select idAccounts from owners where idCustomers= %s """
     cursor.execute(sql, [identity])
-    data = cursor.fetchone()
+    data = cursor.fetchall()
+    accountsTable = []
 
-    userData = []
     for row in data:
-        userData.append(row)
+        accountsTable.append(row[0])
 
-    return {
-        'idCustomer': identity,
-        'login': userData[0],
-        'firstName': userData[1],
-        'lastName': userData[2],
-        'email': userData[3],
-        'phone': userData[4],
-        'dataBecomeCustomer': userData[5]
-    }
+    myJson = []
+    for id in accountsTable:
+
+        sql = """select number, dataOpened, balance from accounts where idAccounts= %s """
+        cursor.execute(sql, [id])
+        data = cursor.fetchone()
+        userData = []
+        for row in data:
+            userData.append(row)
+
+        myJson.append({
+            'idAccounts': id,
+            'number': userData[0],
+            'dataOpened': userData[1],
+            'balance': userData[2]
+        })
+
+    return jsonify(myJson)
 
