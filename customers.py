3b7edcad126e6f18e0f0b86699c3a7c49c81ca71
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-27 10:22:10 +0100

    obsługa customers tylko dla zalogowanych

--- customers.py
+++ customers.py
@@ -12,7 +12,7 @@ customersblueprint = Blueprint('customersblueprint', __name__)
 def add_claims_to_access_token(identity):
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute("""select idCustomer, firstName, lastName, email, phone, dateBecomeCustomer from customers where idCustomers= '%s' """, [identity])
+    cursor.execute("""select idCustomer, firstName, lastName, email, phone, dateBecomeCustomer from customers where login= '%s' """, [identity])
     data = cursor.fetchone()
     idCustomer = data[0]
     firstName = data[1]
