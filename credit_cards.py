2bd29b9baf6908954dcd017d149d5e439295d77b
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-27 18:50:21 +0100

    usprawnie api dla kart

--- credit_cards.py
+++ credit_cards.py
@@ -4,46 +4,31 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 credit_cardsblueprint = Blueprint('credit_cardsblueprint', __name__)
 
-
+# wszystkie karty przypisane do danego użytkownika
 @credit_cardsblueprint.route('/credit_cards', methods=['GET'])
 @jwt_required
 def credit_cards():
     identity = get_jwt_identity()
-
     accountsIDs = getIdsAccountsOfCustomer(identity)
-    # wpisanie do tablicy wszyskich informacji o koncie o danym id
-    myJson = []
-    conn = mysql.connect()
-    cursor = conn.cursor()
 
     idCards = []
     for id in accountsIDs:
         idCards = idCards + getIdsCreditCardsOfAccount(id)
 
-    for id in idCards:
-        sql = """select idAccounts, maximumLimit, expiryDate from credit_cards where idCreditCards= %s """
-        cursor.execute(sql, [id])
-        data = cursor.fetchone()
+    return getInfoAboutCards(idCards)
 
-        userData = []
-        for row in data:
-            userData.append(row)
-
-        myJson.append({
-            'idCreditCards': id,
-            'idAccounts': userData[0],
-            'maximumLimit': userData[1],
-            'expiryDate': userData[2]
-        })
-    return jsonify(myJson)
-
+# wszystkie karty przypisane do danego konta
 @credit_cardsblueprint.route('/credit_cards/<int:idAccount>', methods=['GET'])
 @jwt_required
 def creditCardsOfAccount(idAccount):
+
+    idCards = getIdsCreditCardsOfAccount(idAccount)
+    return getInfoAboutCards(idCards)
+
+def getInfoAboutCards(idCards):
     myJson = []
     conn = mysql.connect()
     cursor = conn.cursor()
-    idCards = getIdsCreditCardsOfAccount(idAccount)
     for id in idCards:
         sql = """select idAccounts, maximumLimit, expiryDate from credit_cards where idCreditCards= %s """
         cursor.execute(sql, [id])
