235426ba46014ba2b0a776923a21a65a610cee85
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-27 21:48:55 +0100

    testowanie

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
 from flask import Blueprint, jsonify
 from . import mysql
-
+from project.mysqlHandler import accountNumToAccountID
 testblueprint = Blueprint('testblueprint', __name__)
 
 @testblueprint.route('/test')
 def test():
-    cursor = mysql.get_db().cursor()
-    SQL="select * from accounts"
-    cursor.execute(SQL)
-    data=cursor.fetchall()
-    resp=jsonify(data)
-    return resp
+
+
+    return jsonify(accountNumToAccountID(5465565656656565))
     
 
    
