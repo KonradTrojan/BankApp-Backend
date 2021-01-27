d5aaf7a800bbe1d05de0f9a19fa067a53f1d8ea8
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-27 22:42:39 +0100

    dodanie transakcji - testowanie

--- transfer.py
+++ transfer.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from project.mysqlHandler import mysql, isOwner, accountNumToAccountIDt
+from project.mysqlHandler import mysql, isOwner, accountNumToAccountID
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from datetime import datetime
 transferBlueprint = Blueprint("transferBlueprint", __name__)
