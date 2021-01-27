778b312a310844910e0e6c78a7732c082d16e7bd
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-27 16:45:20 +0100

    json z info o zalogowanym uzytkowniku

--- customers.py
+++ customers.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request, session, json
 from . import mysql
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, get_jwt_identity
 from project.jwtHandler import jwt, blacklist
 
 customersblueprint = Blueprint('customersblueprint', __name__)
@@ -25,27 +25,25 @@ def customersForId():
         cursor.execute(sql, [id])
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
     else:
-        claims = get_jwt_claims()
-        return claims, 200
+        identity = get_jwt_identity
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        sql = """select login, firstName, lastName, email, phone, dateBecomeCustomer from customers where idCustomers= %s """
+        cursor.execute(sql, [identity])
+        data = cursor.fetchone()
 
-@jwt.user_claims_loader
-def add_claims_to_access_token(identity):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    sql = """select login, firstName, lastName, email, phone, dateBecomeCustomer from customers where idCustomers= %s """
-    cursor.execute(sql, [identity])
-    data = cursor.fetchone()
+        userData = []
+        for row in data:
+            userData.append(row)
 
-    userData = []
-    for row in data:
-        userData.append(row)
+        return {
+            'idCustomer': identity,
+            'login': userData[0],
+            'firstName': userData[1],
+            'lastName': userData[2],
+            'email': userData[3],
+            'phone': userData[4],
+            'dataBecomeCustomer': userData[5]
+        }
 
-    return {
-        'idCustomer': identity,
-        'login': userData[0],
-        'firstName': userData[1],
-        'lastName': userData[2],
-        'email': userData[3],
-        'phone': userData[4],
-        'dataBecomeCustomer': userData[5]
-    }
+
