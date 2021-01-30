bd6c361a0d5ddcb1e5639501e6fddac72ba15d71
Konrad Trojan <konrad.trojan99@gmail.com>
2021-01-30 15:14:15 +0100

    dodanie dodawania kart

--- credit_cards.py
+++ credit_cards.py
@@ -1,17 +1,16 @@
 from flask import Blueprint, jsonify, request
 from project.mysqlHandler import mysql, getIdsAccountsOfCustomer, getIdsCreditCardsOfAccount, isOwner
 from flask_jwt_extended import jwt_required, get_jwt_identity
+import datetime
 
 credit_cardsblueprint = Blueprint('credit_cardsblueprint', __name__)
 
 # wszystkie karty przypisane do danego użytkownika
-@credit_cardsblueprint.route('/credit_cards', methods=['GET','DELETE'])
+@credit_cardsblueprint.route('/credit_cards', methods=['GET','DELETE','POST'])
 @jwt_required
 def credit_cards():
     if request.method == 'GET':
-
         accountsIDs = getIdsAccountsOfCustomer(get_jwt_identity())
-
         idCards = []
         for id in accountsIDs:
             idCards = idCards + getIdsCreditCardsOfAccount(id)
@@ -32,7 +31,7 @@ def credit_cards():
         idAcc = getAccountIdOfCard(idCard)
 
         if not isinstance(idAcc, int):
-            return jsonify({"msg": "Błędne id karty"}), 401
+            return jsonify({"msg": "Nie ma takiej karty"}), 401
 
         if not isOwner(get_jwt_identity(), idAcc):
             return jsonify({"msg": "Brak dostępu"}), 401
@@ -56,7 +55,38 @@ def credit_cards():
         finally:
             cursor.close()
             conn.close()
-            return jsonify({'msg': "Transakcja zakończona pomyślnie"}), 200
+            return jsonify({'msg': "Karta usunięta pomyślnie"}), 200
+
+    elif request.method == 'POST':
+
+        if not request.is_json:
+            return jsonify({"msg": "Missing JSON in request"}), 400
+
+        idAcc = request.json['idAccount']
+
+        if not isOwner(get_jwt_identity(), idAcc):
+            return jsonify({"msg": "Brak dostępu"}), 401
+
+        # rozpoczęcie transakcji
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+
+            # Dodawanie karty do bd
+            sql = """INSERT INTO credit_cards (maximumLimit, expireDate, idAccounts ) VALUES (%s, %s, %s)"""
+            cursor.execute(sql, [5000, datetime.datetime.now() + datetime.timedelta(days=2*365), idAcc])
+
+            # commit zmian
+            conn.commit()
+
+        except mysql.connect.Error as error:
+            # przy wystąpieniu jakiegoś błędu, odrzucenie transakcji
+            cursor.rollback()
+            return jsonify({'msg': "Transakcja odrzucona", 'error': error}), 401
+        finally:
+            cursor.close()
+            conn.close()
+            return jsonify({'msg': "Karta dodane pomyślnie"}), 200
 
 
 # wszystkie karty przypisane do danego konta
